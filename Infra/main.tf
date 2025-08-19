1ef25f9f4310b9ff10013cbf9222fd6846e1515c
kapil <kapildeshmukh27@gmail.com>
2025-08-19 15:10:12 +0530

    tf changes

--- Infra/main.tf
+++ Infra/main.tf
@@ -4,21 +4,27 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-# 2. App Service Plan (Free F1)
-resource "azurerm_service_plan" "asp" {
-  name                = var.asp_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  sku_name            = var.sku
+## 2. App Service Plan (Free F1)
+#resource "azurerm_service_plan" "asp" {
+#  name                = var.asp_name
+#  resource_group_name = azurerm_resource_group.rg.name
+#  location            = azurerm_resource_group.rg.location
+#  os_type             = "Linux"
+#  sku_name            = var.sku
+#}
+
+# Data source for existing App Service Plan
+data "azurerm_app_service_plan" "asp" {
+  name                = "asp-dotnetapi-dev"
+  resource_group_name = "rg-dotnetapi-dev"
 }
 
 # 3. Web App
 resource "azurerm_linux_web_app" "app" {
   name                = var.app_name
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  service_plan_id     = azurerm_service_plan.asp.id
+  location            = data.azurerm_app_service_plan.asp.location
+  service_plan_id     = data.azurerm_app_service_plan.asp.id
 
   site_config {
     application_stack {
